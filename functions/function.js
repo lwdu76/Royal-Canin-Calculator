558efb3f791cc1d970e5a386b70fe219f8f731ce
lwdu76 <laine.william01@gmail.com>
2022-10-19 14:19:08 +0200

    update

--- functions/function.js
+++ functions/function.js
@@ -75,7 +75,7 @@ function age_human_animal () {
 
     for (var i = 0; i < cat_age.length; i++) {
         
-        document.write(" Age du Chat : " + cat_age[i] + "donne en age Humain : " + human_age[i] + "\n");
+        document.write(" Age du Chat : " + cat_age[i] + "donne en Humain : " + human_age[i] + "\n");
     }
 }
 
