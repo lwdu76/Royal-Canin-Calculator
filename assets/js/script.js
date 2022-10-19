038662b5194afdad3672817e5819d19bee92869a
Baptiste Lecordier <lecordierbaptiste@gmail.com>
2022-10-19 14:58:52 +0200

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,33 @@ document.getElementById('btn-dog').addEventListener("click", function(){
 
 });
 
-setInterval(function(){ 
-    console.log('faz')
-}, 500);
+// function validate()
+// {
+//   var phoneNumber = document.getElementById('phone-number').value;
+//   var phoneRGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+//   var phoneResult = phoneRGEX.test(phoneNumber);
+//   alert("phone:"+phoneResult );
+// }
+
+// var inputEmpty = 0
+
+
+
+
+// setInterval(function(){ 
+
+//     if (document.getElementById('name-form').value.length == 0)
+//         inputEmpty = 1;
+//         console.log(document.getElementById('name-form').value.length)
+//     // if
+//     // document.getElementById('my-input-id').disabled = false;
+// }, 500);
+
+
+document.getElementById('form-valid').addEventListener("click", function(){ 
+    document.querySelector('.index-body__div-2').classList.add('hide');
+    document.querySelector('.index-body__div-2').classList.remove('show');
+    document.querySelector('.index-body__div-3').classList.add('show');
+    document.querySelector('.index-body__div-3').classList.remove('hide');
+    console.log('fv')
+});
