23c9641051f8638dde9103f46865dd38d80f0505
lwdu76 <laine.william01@gmail.com>
2022-10-21 12:18:44 +0200

    script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,18 +70,6 @@ for (var slideBtnRight of slidersBtnRight) {
         }   
     }); 
 }
-
-
-
-
-
-
-
-
-
-
-
-
 function breed_drop_down_list () {
     var selected_list = document.getElementById("frace");
     cat_list = new Array ("abyssin", "american curl", "american shorthair", "american wirehair", "anatoli", "angora turc", "asian", "brume australianne", "balinais", "bengal", "bleu russe", "americain", "bobtail des kouriles", "bobtail japonais", "bombay", "brazilian shorthair", "british longhair", "british shorthair", "burmese", "burmilla", "californian rex", "californian spangled", "ceylan", "chartreux", "chantilly", "chausie", "colorpoint shortair", "cornish rex", "cymric", "devon rex", "donskoy", "european shortair", "exotic shortair", "german rex", "havana brown", "highlander", "highland fold", "himalayen", "khao manee", "korat", "laperm", "maine coon", "mandarin", "manx", "mau arabe", "mau égyptien", "minskin", "munchkin", "nebelung", "norvégien ou skogkatt", "ocicat", "ojos azules", "oriental shortair", "persan", "peterbald", "pixie-bob", "ragamuffin", "ragdoll", "sacré de bismanie", "safari", "savannah", "scottish fold", "selkirk rex", "serengeti", "seychellois", "siamois", "sibérien", "skookum", "snowshoe", "sokoké", "somali", "sphynx", "thaï", "tiffany", "tonkonois", "toyger", "turc de van", "ural rex", "york chocolat");
@@ -113,9 +101,6 @@ age_drop_down_list ();
 function age_human_animal () {
     human_age = new Array ("1 an", "2 ans", "4 ans", "6 ans", "8 ans", "10 ans", "12 ans", "14 ans", "15 ans", "16 ans", "17 ans", "19 ans", "24 ans", "28 ans", "32 ans", "36 ans", "40 ans", "44 ans", "48 ans", "52 ans", "56 ans", "60 ans", "64 ans", "68 ans", "72 ans", "76 ans", "80 ans", "84 ans", "88 ans", "94 ans", "100 ans");
     cat_age = new Array ("1 mois", "2 mois", "3 mois", "4 mois", "5 mois", "6 mois", "7 mois", "8 mois", "9 mois", "10 mois", "11 mois", "1 an", "2 ans", "3 ans", "4 ans", "5 ans", "6 ans", "7 ans", "8 ans", "9 ans", "10 ans", "11 ans", "12 ans", "13 ans", "14 ans", "15 ans", "16 ans", "17 ans", "18 ans", "19 ans", "20 ans");
-    // for (var i = 0; i < cat_age.length; i++) {
-    //     document.write(" Age du Chat : " + cat_age[i] + "donne en Humain : " + human_age[i] + "\n");
-    // }
 }
 
 age_human_animal ();
@@ -175,25 +160,21 @@ function formChecker() {
         checkGoodAnswer3 = true;
     } else {
         checkGoodAnswer3 = false;
-    }
+}
+        
 
-    if (checkGoodAnswer && checkFormName2 && checkFormName3) {
-        document.getElementById('submit-btn-r').classList.remove('d-none')
-    } else {
-        // document.getElementById('submit-btn-r').classList.add('d-none')
-    }
+    // if (!checkGoodAnswer && !checkGoodAnswer2 && !checkGoodAnswer3) {
+    //     console.log('true');
+    //     document.getElementById('submit-btn-r').classList.add('slider-btn-right')
+    // } else {
+    //     console.log('false')
+    //     document.getElementById('submit-btn-r').classList.remove('slider-btn-right')
+    // }
     
 
+}
 
 
-document.getElementById('download-btn').addEventListener("click", function(){
-domtoimage.toJpeg(document.querySelector('.img-result-age'), { quality: 0.95 })
-    .then(function (dataUrl) {
-        var link = document.createElement('a');
-        link.download = 'my-image-name.jpeg';
-        link.href = dataUrl;
-        link.click();
-    });
 
 
 document.getElementById('download-btn').addEventListener("click", function(){
@@ -300,14 +281,4 @@ function formResults() {
 
 }
 
-formResults()
-
-
-
-
-
-
-
-
-
-
+formResults();
