31afcd964292e01979b22541e9e4a3b02ecc1678
lwdu76 <laine.william01@gmail.com>
2022-10-22 08:50:52 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -174,17 +174,44 @@ domtoimage.toJpeg(document.querySelector('.img-result-age'), { quality: 0.95 })
 
 setInterval(formResults, 100);
 
+function Products (image, name_kibble, price, age_recommandation, type_kibble) {
 
-var resulttxt
+this.image = image;
+    
+this.name = name_kibble;
+
+this.price = price;
+
+this.age_recommandation = age_recommandation;
+
+this.type_kibble = type_kibble;
+
+}
+
+const product_1 = new Product_cat ("img/kibble_1.jpg", 'Mother & Babycat pour chatte et chaton', 25, '0 - 4 mois', 'sec et humide');
+const product_2 = new Product_cat ('img/kibble_2.jpg', 'Babycat milk', 18.49, '0 - 4 mois', 'humide');
+const product_3 = new Product_cat ('img/kibble_3.jpg', 'kitten pour chaton', 26.10, '5 mois - 1 an', 'sec');
+const product_4 = new Product_cat ('img/kibble_4.jpg', 'kitten Gelée', 28.60, '5 mois - 1 an', 'humide');
+const product_5 = new Product_cat ('img/kibble_5.jpg', 'Maine Coon Adulte', 26.09, '1 - 12 ans', 'sec');
+const product_6 = new Product_cat ("img/kibble_6.jpg", 'Instinctive en gelée', 26.02, '1 - 7 ans', 'humide');
+const product_7 = new Product_cat ('img/kibble_7.jpg', 'Appetite controle Care en gelée', 31.20, '7 - 12 ans', 'humide');
+const product_8 = new Product_cat ('img/kibble_8.jpg', 'Ageing 12 +', 22.99, '+ 12 ans', 'sec');
+const product_9 = new Product_cat ("img/kibble_9.jpg", 'Ageing 12 + en sauce', 28.60, '+ 12 ans', 'humide');
+
+let products = [];
+
+products.push (product_1, product_2, product_3, product_4, product_5, product_6, product_7, product_8, product_9);
+
+console.log(products);
+
+var resultat_text
 function formResults() {
 
 // Sélection de produits
-    resulttxt = parseInt(document.getElementById('fage').value);
-    if (Number.isInteger(resulttxt)) {
-        document.querySelector('#results-calcul').innerHTML = (document.getElementById('fname').value+' a '+ human_age[resulttxt]+' en âge humain')
+    resultat_text = parseInt(document.getElementById('fage').value);
+    if (Number.isInteger(resultat_text)) {
+        document.querySelector('#results-calcul').innerHTML = (document.getElementById('fname').value+' a '+ human_age[resultat_text]+' en âge humain')
     }
-
-    console.log(document.getElementById('fage').value)
 
     if (document.getElementById('fage').value !== NaN) {
         if (parseInt(document.getElementById('fage').value) < 5) {
