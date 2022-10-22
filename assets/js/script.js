f57a37fabd8c8fb1d01e9ec1e8382d3ada126d3b
lwdu76 <laine.william01@gmail.com>
2022-10-22 08:03:52 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -100,12 +100,12 @@ function age_drop_down_list () {
 
 age_drop_down_list ();
 
-function age_human_animal () {
+/*function age_human_animal () {
     human_age = new Array ("1 an", "2 ans", "4 ans", "6 ans", "8 ans", "10 ans", "12 ans", "14 ans", "15 ans", "16 ans", "17 ans", "19 ans", "24 ans", "28 ans", "32 ans", "36 ans", "40 ans", "44 ans", "48 ans", "52 ans", "56 ans", "60 ans", "64 ans", "68 ans", "72 ans", "76 ans", "80 ans", "84 ans", "88 ans", "94 ans", "100 ans");
     cat_age = new Array ("1 mois", "2 mois", "3 mois", "4 mois", "5 mois", "6 mois", "7 mois", "8 mois", "9 mois", "10 mois", "11 mois", "1 an", "2 ans", "3 ans", "4 ans", "5 ans", "6 ans", "7 ans", "8 ans", "9 ans", "10 ans", "11 ans", "12 ans", "13 ans", "14 ans", "15 ans", "16 ans", "17 ans", "18 ans", "19 ans", "20 ans");
 }
 
-age_human_animal ();
+age_human_animal ();*/
 
 setInterval(formChecker, 100);
 
