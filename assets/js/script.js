c05cbad1d3613fde8f6a8c98d31a9635c663e938
lwdu76 <laine.william01@gmail.com>
2022-10-22 08:53:50 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -188,15 +188,15 @@ this.type_kibble = type_kibble;
 
 }
 
-const product_1 = new Product_cat ("img/kibble_1.jpg", 'Mother & Babycat pour chatte et chaton', 25, '0 - 4 mois', 'sec et humide');
-const product_2 = new Product_cat ('img/kibble_2.jpg', 'Babycat milk', 18.49, '0 - 4 mois', 'humide');
-const product_3 = new Product_cat ('img/kibble_3.jpg', 'kitten pour chaton', 26.10, '5 mois - 1 an', 'sec');
-const product_4 = new Product_cat ('img/kibble_4.jpg', 'kitten Gelée', 28.60, '5 mois - 1 an', 'humide');
-const product_5 = new Product_cat ('img/kibble_5.jpg', 'Maine Coon Adulte', 26.09, '1 - 12 ans', 'sec');
-const product_6 = new Product_cat ("img/kibble_6.jpg", 'Instinctive en gelée', 26.02, '1 - 7 ans', 'humide');
-const product_7 = new Product_cat ('img/kibble_7.jpg', 'Appetite controle Care en gelée', 31.20, '7 - 12 ans', 'humide');
-const product_8 = new Product_cat ('img/kibble_8.jpg', 'Ageing 12 +', 22.99, '+ 12 ans', 'sec');
-const product_9 = new Product_cat ("img/kibble_9.jpg", 'Ageing 12 + en sauce', 28.60, '+ 12 ans', 'humide');
+const product_1 = new Product_cat ("./assets/img/kibble_1.jpg", 'Mother & Babycat pour chatte et chaton', 25, '0 - 4 mois', 'sec et humide');
+const product_2 = new Product_cat ('./assets/img/kibble_2.jpg', 'Babycat milk', 18.49, '0 - 4 mois', 'humide');
+const product_3 = new Product_cat ('./assets/img/kibble_3.jpg', 'kitten pour chaton', 26.10, '5 mois - 1 an', 'sec');
+const product_4 = new Product_cat ('./assets/img/kibble_4.jpg', 'kitten Gelée', 28.60, '5 mois - 1 an', 'humide');
+const product_5 = new Product_cat ('./assets/img/kibble_5.jpg', 'Maine Coon Adulte', 26.09, '1 - 12 ans', 'sec');
+const product_6 = new Product_cat ("./assets/img/kibble_6.jpg", 'Instinctive en gelée', 26.02, '1 - 7 ans', 'humide');
+const product_7 = new Product_cat ('./assets/img/kibble_7.jpg', 'Appetite controle Care en gelée', 31.20, '7 - 12 ans', 'humide');
+const product_8 = new Product_cat ('./assets/img/kibble_8.jpg', 'Ageing 12 +', 22.99, '+ 12 ans', 'sec');
+const product_9 = new Product_cat ("./assets/img/kibble_9.jpg", 'Ageing 12 + en sauce', 28.60, '+ 12 ans', 'humide');
 
 let products = [];
 
